12e3dab80587795392fd21ba1e02fb21889e958a
zk-blde <570516599@qq.com>
2022-12-19 10:42:57 +0800

    feat: 新增工厂, 抽象工厂, 生成器模式

---
+++ abstract_factory/iSportsFactory.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// 接口层
+
+type ISportsFactory interface {
+	makeShoe() IShoe
+	makeShirt() IShirt
+}
+
+func GetSportsFactory(brand string) (ISportsFactory, error) {
+	if brand == "adidas" {
+		return &Adidas{}, nil
+	}
+
+	if brand == "nike" {
+		return &Nike{}, nil
+	}
+
+	return nil, fmt.Errorf("Wrong brand type passed")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module Design
+
+go 1.18
