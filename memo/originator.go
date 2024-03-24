5d27e445e1b58601d06bbcdf7c17d0ab3b9456d1
“zk_blde” <570516599@qq.com>
2024-03-25 00:58:27 +0800

    feat: 添加备忘录模式

---
+++ memo/originator.go
@@ -0,0 +1,23 @@
+package main
+
+// 原发器
+
+type Originator struct {
+	state string
+}
+
+func (e *Originator) createMemento() *Memento {
+	return &Memento{state: e.state}
+}
+
+func (e *Originator) restoreMemento(m *Memento) {
+	e.state = m.getSavedState()
+}
+
+func (e *Originator) setState(state string) {
+	e.state = state
+}
+
+func (e *Originator) getState() string {
+	return e.state
+}
